3653d484c1e6f8b1fb443010632fef4832b546a0
Osuide clement <debaosuidecl@gmail.com>
2020-05-22 22:31:02 +0100

    D

---
+++ client/build/precache-manifest.29371f01026b33721fae93e1c22168e0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6558af49e950efd02f80925136da01bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a90cfc5f1fcf101cac75",
+    "url": "/static/css/main.0dc8dbc2.chunk.css"
+  },
+  {
+    "revision": "7057eeb2b9cf30965b7e",
+    "url": "/static/js/2.bf4fd6e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bf4fd6e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a90cfc5f1fcf101cac75",
+    "url": "/static/js/main.0c353f82.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "c2c1cd14d8b5fc3add2cae422917d820",
+    "url": "/static/media/logo.c2c1cd14.png"
+  },
+  {
+    "revision": "3d29737e22306ab357afd7e100e15c7d",
+    "url": "/static/media/modalimage.3d29737e.png"
+  },
+  {
+    "revision": "052e743016f8760a2713744261d563c3",
+    "url": "/static/media/regpage2.052e7430.png"
+  },
+  {
+    "revision": "e5d9202e62366805af88dfc49c9df9e1",
+    "url": "/static/media/regpage3.e5d9202e.png"
+  },
+  {
+    "revision": "e4a1fe37c63067cded0207528346a6f1",
+    "url": "/static/media/signinimage.e4a1fe37.png"
+  },
+  {
+    "revision": "c74f304fec91de7e90d16905b7471da8",
+    "url": "/static/media/signupimagepage1.c74f304f.png"
+  }
+]);
